d7309e26c3847bf7b0d6ea4387b2b20af7047fe5
shahanas <smshan065@gmail.com>
2021-04-19 00:30:24 +0530

    save to git

--- company/emp/models.py
+++ company/emp/models.py
@@ -13,17 +13,23 @@ class employee(models.Model):
     email=models.CharField(max_length=90)
     skill=models.CharField(max_length=50)
     roll=models.CharField(max_length=1,choices=Roll)
+    class Meta:
+         db_table = "employee"  
 
  
 
 class skills(models.Model):
    
     skill=models.ManyToManyField(employee)
+    class Meta:
+         db_table = "skill"
    
 
 class team(models.Model):
     team_name=models.CharField(max_length=50)
     team_leader_id=models.ForeignKey(employee,  on_delete=models.CASCADE)
+    class Meta:
+         db_table = "team"
 
 
 
--- company/emp/views.py
+++ company/emp/views.py
@@ -6,31 +6,29 @@ from .forms import EmployeeRegistration
 from .models import employee
 # Create your views here.
 def addemployee(request):
-    
     emp = employee.objects.all()
     fm = EmployeeRegistration()
     return render(request,'enroll/base.html',{'form':fm,'em': emp})
 #@csrf_exempt
 def save_data(request):
-    skill = request.POST.get('skills')
     form= EmployeeRegistration(request.POST)
     if request.method=="POST":
         if form.is_valid():
-            
             name= request.POST['name']
             email = request.POST['email']
             skill = request.POST['skill']
             roll = request.POST['roll']
-            usr = employee(name=nm,email=em,skill=sk,roll=rl)
-            usr.save()
+            usr = employee(name=name,email=email,skill=skill,roll=roll)
+            form.save()
             emp = employee.objects.values()
             print(emp)
             employee_data = list(emp)
-            
-            return JsonResponse({'status' : 'save' ,'employee_data':employee_data})
-    
+            return JsonResponse({'status' : 'save' ,'employee_data': employee_data})
         else:
             return JsonResponse({'status' : 0})
-
-
-    
+           
+           
+           
+           
+           
+        
